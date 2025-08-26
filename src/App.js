463087fcaf99d086fed3348279a325c05d4b7bb9
FelipeAugust0 <lipe.augusto2013@gmail.com>
2025-08-25 22:00:51 -0300

    NEW: colocada variavel de estado valor e X responde evendo com click

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 // Criação do elemento square que irá compor o tabuleiro
+import { useState } from 'react';
 import './App.css';
 
-function Square({valor}){
+function Square(){
+  const [valor, setValor] = useState(null);
+
   function handleClick(){
-    console.log("Botão clicado!")
+    setValor('X');
   }
   return(
-    <button className="square" onClick="{handleClick}">{valor}</button>
+    <button className="square" onClick={handleClick}>{valor}</button>
   );
 }
 
@@ -14,19 +17,19 @@ export default function Tabuleiro(){
   return(
     <div>
       <div>
-        <Square valor = "1"/>
-        <Square valor = "2"/>
-        <Square valor = "3"/>
+        <Square/>
+        <Square/>
+        <Square/>
       </div>
       <div>
-        <Square valor = "4"/>
-        <Square valor = "5"/>
-        <Square valor = "6"/>
+        <Square/>
+        <Square/>
+        <Square/>
       </div>
       <div>
-        <Square valor = "7"/>
-        <Square valor = "8"/>
-        <Square valor = "9"/>
+        <Square/>
+        <Square/>
+        <Square/>
       </div>
     </div>
   )
